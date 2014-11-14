e74c6e1df15f8119c6a7f702465c7c4cc1321d51
Harry Copeman <harry.copeman@gmail.com>
2014-11-14 09:24:41 +0000

    Removed <br>

--- about/index.php
+++ about/index.php
@@ -13,9 +13,9 @@ include ("../include/header.php");
 
             <p>I’m currently freelancing, solving various challenges for my clients. Previous to this, I was the co-founder of a digital studio we called <a href="http://cyclestudios.co/" target="_blank">Cycle Studios</a>. I’m now looking to join a team full time — you can have a read of Résume <a href="https://www.dropbox.com/s/w38vg5upwhm74eq/Harry%20Copeman%20R%C3%A9sum%C3%A9.pdf" target="_blank">here</a>, or take a look at <a href="../portfolio">my work</a>.</p>
 
-            <p>Away from the screen I spend most of my time with my girlfriend Cara, or turning the gears on my bike through the countryside.<br><br>
+            <p>Away from the screen I spend most of my time with my girlfriend Cara, or turning the gears on my bike through the countryside.</p>
 
-              Feel free to stalk me on <a href="https://twitter.com/harrycopeman">Twitter</a>, or stay up to date with what I’m working on via <a href="https://dribbble.com/harrycopeman">Dribbble</a>. I’m always up for a chat too - <a href="mailto:harry.copeman@gmail.com">harry.copeman@gmail.com</a>.</p>
+              <p>Feel free to stalk me on <a href="https://twitter.com/harrycopeman">Twitter</a>, or stay up to date with what I’m working on via <a href="https://dribbble.com/harrycopeman">Dribbble</a>. I’m always up for a chat too - <a href="mailto:harry.copeman@gmail.com">harry.copeman@gmail.com</a>.</p>
 
             <p>Take it easy.</p>
           </div>
