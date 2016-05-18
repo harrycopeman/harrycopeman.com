18dc5413b00bc438afafc568928d949c49b9b051
Harry Copeman <harry.copeman@gmail.com>
2016-05-18 14:10:16 +0100

    Final Video

--- portfolio/mailbox/index.php
+++ portfolio/mailbox/index.php
@@ -44,7 +44,12 @@ include ("../../_include/header.php");
               <p>With some help from Ryan Putman on this amazing set of icons, we had a page to be pretty excited about. We also brought the sites other pages up to scratch with the help section.</p>
 
             <img src="../../_core/imgs/work/mailbox/3.png"/>
-            <img src="../../_core/imgs/work/mailbox/help.png"/>
+
+            <video width="100%" controls>
+              <source src="../../_core/video/mailbox.mp4" type="video/mp4">
+              <source src="../../_core/video/mailbox.ogg" type="video/ogg">
+             Your browser does not support the video tag.
+            </video>
 
             <p>With the help of <a target='_blank' href='https://twitter.com/kevinewe\'>Kevin</a> I also put the code together for the new help section. By using AJAX we make it easy for the user operations team to add further languages down the line. The rest of the site was HTML, CSS and JavaScript.</p>
 
