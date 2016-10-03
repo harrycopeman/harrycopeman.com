c5b29bdf4ea34c2c2d7b768b87a468984e199f5f
Harry Copeman <harry.copeman@gmail.com>
2016-10-03 20:00:29 +0100

    fix

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include ("_include/header.php");
 
 				<h1>👋 Hey, I'm Harry Copeman - a product designer at Facebook London, making good things with good people. </h1>
 
-				<p>Previous to Facebook I freelaned for various companies including Dropbox, SeatGeek and Switch. I was the winner of the D&AD New Blood Award in 2012, and was recently nominated as Net Awards Young Designer of the Year.<br><br>
+				<p>Previous to Facebook I freelanced for various companies including Dropbox, SeatGeek and Switch. I was the winner of the D&AD New Blood Award in 2012, and was recently nominated as Net Awards Young Designer of the Year.<br><br>
 
 				If you want to chat design, bikes, or anything else then drop me a line on <a href="https://twitter.com/harrycopeman">harry.copeman@gmail.com</a>✌️<p>
 
