623df0c98eb03aab8b9ea03f03042c24c05eb398
Harry Copeman <harry.copeman@gmail.com>
2016-10-06 17:52:34 +0100

    NEW

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $title = "Harry Copeman";
+$id = "black";
 include ("_include/header.php");
 ?>
 
@@ -7,11 +8,19 @@ include ("_include/header.php");
 	<div class="span">
 		<div class="loop-12">
 
-				<h1>👋 Hey, I'm Harry Copeman - a product designer at Facebook London, making good things with good people. </h1>
 
-				<p>Previous to Facebook I freelanced for various companies including Dropbox, SeatGeek and Switch. I was the winner of the D&AD New Blood Award in 2012, and was recently nominated as Net Awards Young Designer of the Year.<br><br>
 
-				If you want to chat design, bikes, or anything else then drop me a line on <a href="https://twitter.com/harrycopeman">harry.copeman@gmail.com</a>✌️<p>
+				<h2>👋<br><br>I'm Harry - a product designer living in London, designing and prototyping good things with good people.<br><br>
+				
+				I'm currently working at <a href="https://www.facebook.com/">Facebook</a> where I've spent over a year designing tools to help small businesses succeed. I've also designed and supported <a href="https://facebook.github.io/origami/">internal</a> and <a href="http://facebook.design/">external</a> design tools that look to improve designers workflows.<br><br>
+
+				Previous to my time at Facebook, I freelanced for various companies including <a href="https://www.dropbox.com/">Dropbox</a>, where I designed and built part of the <a href="http://www.mailboxapp.com/">Mailbox</a> website to help them scale across new platforms. Other teams that I've worked with include <a href="https://seatgeek.com/">SeatGeek</a> and Switch.<br><br>
+
+				In 2012 I managed to win the D&AD New Blood Award, and in 2015 I was nominated for the Net Awards Young Designer of the Year.<br><br>
+
+				If you want to chat about design, bikes, or anything else then drop me an email.<br><br>
+
+				<h2>📬 harry.copeman@gmail.com<br><br>☎️ 07426895304</h2>
 
 
 
